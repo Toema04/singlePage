52f4d1075401651aa96f9b6c54beb4774c784a07
Toema04 <s.toema04@gmail.com>
2024-02-20 11:51:35 +0200

    coming soon

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./app.css"
+import SiglePage from "./compenent/SiglePage"
+function App() {
+
+  return (
+    <div className="App">
+      <SiglePage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/compenent/SiglePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./siglePage.css";
+
+function SiglePage() {
+    return (
+		<div className="page">
+			Coming Soon...
+            </div>
+	);
+}
+
+export default SiglePage;
